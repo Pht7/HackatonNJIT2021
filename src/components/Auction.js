7e75e641213a8e2abfbdf1cc96f4b56cc36bce15
yarmout <58712447+yarmout@users.noreply.github.com>
2021-11-13 16:41:08 -0500

    Add additional fields to auction page

--- src/components/Auction.js
+++ src/components/Auction.js
@@ -1,20 +1,42 @@
 import React, { useState } from 'react';
 import Bid from "./Bid";
+import importImg from "./Gibbon.jpg";
 
-    var admin = require("firebase-admin");
-
-    var serviceAccount = require("../hacknjit2021-firebase-adminsdk-dcz1m-53e3a25ff7.json");
-
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: "https://hacknjit2021-default-rtdb.firebaseio.com"
-    });
+    // var admin = require("firebase-admin");
+    //
+    // var serviceAccount = require("../hacknjit2021-firebase-adminsdk-dcz1m-53e3a25ff7.json");
+    //
+    // admin.initializeApp({
+    //   credential: admin.credential.cert(serviceAccount),
+    //   databaseURL: "https://hacknjit2021-default-rtdb.firebaseio.com"
+    // });
 
 export default function Auction() {
-    const animal_one = {"type":"Monke", "price":"$39", "photo_url":"./sample_photo.jpg"};
-    const animal_two = {"type":"Horse", "price":"$39", "photo_url":"./sample_photo.jpg"};
-    const animal_three = {"type":"Sheep", "price":"$39", "photo_url":"./sample_photo.jpg"};
+    //sample data
+    const animal_one = {
+        "type":"Monke",
+        "price":"$39",
+        "photo_url":"./sample_photo.jpg",
+        "current_owner":"iDonated",
+        "time_left": "21h"
+    };
+    const animal_two = {
+        "type":"Horse",
+        "price":"$39",
+        "photo_url":"./sample_photo.jpg",
+        "current_owner":"Joe",
+        "time_left":"9h"
+    };
+    const animal_three = {
+        "type":"Sheep",
+        "price":"$39",
+        "photo_url":"./sample_photo.jpg",
+        "current_owner":"Bill",
+        "time_left":"12h"
+    };
     const animal_list = [animal_one, animal_two, animal_three];
+
+
     const [bid, setBid] = useState(false);
     const [animalObject, setAnimalObject] = useState({});
 
@@ -22,9 +44,12 @@ export default function Auction() {
     function animals(animal) {
         return(
             <div>
-                <p> {animal.photo_url}</p>
-                <p><b> {animal.type} </b></p>
-                <p> {animal.price}</p>
+                <p> {animal.photo_url} </p>
+                <h2> {animal.type} </h2>
+                <p> Current Owner: {animal.current_owner} </p>
+                <p> Time Left: {animal.time_left} </p>
+                <p> Current Bid: ${animal.price} </p>
+
                 <button type="button" onClick={() => {
                     setBid(true);
                     setAnimalObject(animal);
@@ -47,13 +72,5 @@ export default function Auction() {
                 <Bid animal={animalObject}/>
             )}
         </div>
-        // <div>
-        //     <h1> Meet the animals! </h1>
-        //     <div className='container'>
-        //         {animal_list.map((animal) => {
-        //             return animals(animal)
-        //         })}
-        //     </div>
-        // </div>
   );
 }
