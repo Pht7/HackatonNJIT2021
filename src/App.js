79d2fd29b5a5d2f4b2bbb7deabde5dca7bab6e6c
Pht7 <pht7@njit.edu>
2021-11-13 12:51:39 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Hello Test</h1>
+      </header>
+        <a href ="google.com"> <button>Hello</button> </a>
+    </div>
+  );
+}
+
+export default App;
