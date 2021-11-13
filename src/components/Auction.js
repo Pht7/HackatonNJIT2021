1ae2b4712435c47ebad9be3f9f0e519ddd18bd7c
yarmout <58712447+yarmout@users.noreply.github.com>
2021-11-13 15:04:28 -0500

    Add bid prototype page

--- src/components/Auction.js
+++ src/components/Auction.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
 export default function Auction() {
-    const animal_one = {"type":"Pig", "price":"$39", "photo":"./sample_photo.jpg"};
-    const animal_two = {"type":"Horse", "price":"$39", "photo":"./sample_photo.jpg"};
-    const animal_three = {"type":"Sheep", "price":"$39", "photo":"./sample_photo.jpg"};
+    const animal_one = {"type":"Monke", "price":"$39", "photo_url":"./sample_photo.jpg"};
+    const animal_two = {"type":"Horse", "price":"$39", "photo_url":"./sample_photo.jpg"};
+    const animal_three = {"type":"Sheep", "price":"$39", "photo_url":"./sample_photo.jpg"};
     const animal_list = [animal_one, animal_two, animal_three];
 
 
     function animals(animal) {
         return(
             <div>
+                <p> {animal.photo_url}</p>
                 <p><b> {animal.type} </b></p>
-                <p> Buy now! </p>
+                <p> {animal.price}</p>
+                <p> Bid now! </p>
             </div>
         )
     }
