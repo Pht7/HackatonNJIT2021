01c9fc0ea7d936949531ec1208935580c4601491
yarmout <58712447+yarmout@users.noreply.github.com>
2021-11-13 14:37:14 -0500

    Emulate sample database data

--- src/components/Auction.js
+++ src/components/Auction.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 export default function Auction() {
-    const animal_list = ['Pig', 'Sheep', 'Cow'];
+    const animal_one = {"type":"Pig", "price":"$39", "photo":"./sample_photo.jpg"};
+    const animal_two = {"type":"Horse", "price":"$39", "photo":"./sample_photo.jpg"};
+    const animal_three = {"type":"Sheep", "price":"$39", "photo":"./sample_photo.jpg"};
+    const animal_list = [animal_one, animal_two, animal_three];
+
 
     function animals(animal) {
         return(
             <div>
-                <p><b> {animal} </b></p>
+                <p><b> {animal.type} </b></p>
                 <p> Buy now! </p>
             </div>
         )
