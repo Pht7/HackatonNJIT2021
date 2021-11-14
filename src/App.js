821767f63109228be1bcd8094872fc099f6be6c4
yarmout <58712447+yarmout@users.noreply.github.com>
2021-11-14 09:11:15 -0500

    Add my name

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ function Home() {
       <h1>This is our app. We need to come up with a name for it</h1>
       <p> Why did we create this web application? The purpose was to support our local zoos, animal sanctuary, and anti poaching abroad. With our auction bidding system, the funds created would go 100% back to the community to help fight these causes. As a token from these organizations, we have teamed up and the winner of of each auction will get a chance to spend a day with those animals. This includes feeding, petting and much more.</p>
       <p>Created using React, CSS, HTML, and Flask</p>
-      <p>Team members: Peter Tran, Adrian Majcher, Samuel Muller, Yezen </p>
+      <p>Team members: Peter Tran, Adrian Majcher, Samuel Muller, Yezen Armout </p>
     </div>
   );
 }
